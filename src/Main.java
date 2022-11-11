cac2ab3bda5e4c509885e5bdb3d4dcc679cab736
Jskam <svkam.igor@gmail.com>
2022-11-12 04:04:10 +1000

    First commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         GUI gui = new GUI();
 
-//        Game game = new Game();
-//        game.printArray();
+        Game game = new Game();
+        game.printArray();
 //
 //        game.addNewNumbers();
 //        game.printArray();
